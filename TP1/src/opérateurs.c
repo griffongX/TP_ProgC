8536813428b9f57fe4148b1834d390f5b6efca15
griffongX <vargahhy@gmail.com>
2025-11-21 10:08:14 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("Addition a + b = %d\n", a + b);
+    printf("Soustraction a - b = %d\n", a - b);
+    printf("Multiplication a * b = %d\n", a * b);
+    printf("Division a / b = %d\n", a / b);
+    printf("Modulo a %% b = %d\n", a % b);
+
+    printf("a est égal à b ? %d\n", a == b);
+    printf("a est supérieur à b ? %d\n", a > b);
+
+    return 0;
+}
